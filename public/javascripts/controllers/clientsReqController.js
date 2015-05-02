d1ee1425f08dfdc6d95c5319fdc2375ad1eb41af
RohanBhanderi <rohan.bhanderi@sjsu.edu>
2015-05-02 10:39:00 -0700

    Client req and cleanup

--- public/javascripts/controllers/clientsReqController.js
+++ public/javascripts/controllers/clientsReqController.js
@@ -1,5 +1,6 @@
 'use strict';
-wfms.controller("ClientsReqCtrl", function($scope, $rootScope, DataService) {
+wfms.controller("ClientsReqCtrl", function($scope, $rootScope, $modal,
+		$location, DataService) {
 
 	$scope.getAllPNDGClients = function(){
 		DataService.getData(urlConstants.GET_PNDG_CLIENTS,[]).success(function(response){
@@ -12,16 +13,26 @@ wfms.controller("ClientsReqCtrl", function($scope, $rootScope, DataService) {
 		});
 	}
 
-	$scope.getGuardsToAssign = function(){
-		DataService.getData(urlConstants.GET_GUARDS_ASSIGN,[]).success(function(response){
-			if(response.data){
-				console.log(response.data);
-				$scope.guardReqList = response.data;
-			}
-		}).error(function(err){
-			console.log(err.message);
-		});
-	}
+	$scope.assignGuards = function(data) {
+		console.log("did i get called");
 
+		var modalInstance = $modal.open({
+			templateUrl : 'templates/admin/assignGuards.html',
+			controller : 'AssignGuardsCtrl',
+			size : 'lg',
+			resolve : {
+				isEdit : function(){
+					return data;
+				}
+			}
+		});
+
+		modalInstance.result.then(function(isValid) {
+			if (isValid) {
+				getAllPNDGClients();
+			}
+		}, function() {
+		});
+	};
 
 });

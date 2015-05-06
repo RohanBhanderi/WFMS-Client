9b4228b7c4306ada7118b2276c681f89ee06bce2
swatikasat <kasatswati@gmail.com>
2015-05-06 02:14:42 -0700

    clientregistrationcorrected

--- public/javascripts/controllers/clientRegistrationController.js
+++ public/javascripts/controllers/clientRegistrationController.js
@@ -135,7 +135,15 @@ $scope.register = function() {
 				console.log("Inside register Funct"+$scope.firstname);
 				console.log($scope.ssn + $scope.firstname + $scope.lastname + $scope.address.formatted_address + $scope.city + $scope.zipcode + $scope.email + $scope.number +  $scope.password +$scope.state);
 
-		    if(!(($scope.ssn =="" || $scope.firstname =="" || $scope.lastname =="" || $scope.address.formatted_address =="" || $scope.city =="" || $scope.zipcode=="" || $scope.email=="" ||  $scope.number=="" ||  $scope.password=="" || $scope.state==""))){
+		    if($scope.ssn &&
+		    $scope.firstname &&
+		    $scope.lastname && 
+		    $scope.address.formatted_address &&
+		    $scope.city &&
+		    $scope.zipcode && 
+		    $scope.email &&  $scope.number && 
+		    $scope.password &&
+		    $scope.state){
                
 					console.log("Inside query");
 				var params={
@@ -163,39 +171,10 @@ $scope.register = function() {
 			});
 		
 
-		$scope.formError = "Form Invalid !!!";
-	}else{
-		if(!(isValidPostalCode(zipcode))){
-			
-			$scope.formError = "Invalid Zipcode !!!";
-		}
-		else if(!(isSSN(ssnError))){
-			$scope.ssn=" $scope.ssnError=";
-			$scope.formError = "Invalid SSN Format !!!";
-		}
-		else{
-		var params = {
-				
-				fname : $scope.firstname,
-				lname:  $scope.lastname,
-				address : $scope.address,
-				city : $scope.city,
-				zipcode : $scope.zipcode,
-				email : $scope.email,
-				phonenumber : $scope.number,
-				password : $scope.password,
-				ssn : $scope.ssn,
-				usertype: "CLNT"
-					
-			};
-			console.log("Params value:" + JSON.stringify(params));
-		DataService.postData("/api/register",params).success(function(response){
-			$modalInstance.close(true);
-		}).error(function(err){
-			$modalInstance.dismiss(false);
-		});
+		
 	}
-
+		else{
+			$scope.formError = "Form Invalid !!!";
 				
 			}
 		};
